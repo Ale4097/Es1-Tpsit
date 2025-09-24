7b1f7964204caf9d1c521d1c286e04bca9d1664a
lai2-24 <lai2-24@itismeucci.com>
2025-09-24 09:28:01 +0200

    secondo commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,16 @@
 package com.example;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         System.out.println("Hello world!");
+        System.out.println(Thread.currentThread().getName());
+        
+        MioThread pippo = new MioThread(198);
+        pippo.start();
+        MioThread pluto = new MioThread(195);
+        pluto.start();
+        pippo.join();
+        pluto.join();
+        System.out.println("finito");
     }
 }
